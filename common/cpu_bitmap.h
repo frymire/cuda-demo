211e47fb6a7bb80bc877276834a2d11381098d36
Mark Frymire <mark.frymire@icloud.com>
2022-09-16 10:43:23 -0400

    Remove hard-coded paths from project settings

--- common/cpu_bitmap.h
+++ common/cpu_bitmap.h
@@ -21,67 +21,67 @@
 
 struct CPUBitmap {
 
-	unsigned char *pixels;
-	int x, y;
-	void *dataBlock;
-	void(*bitmapExit)(void*);
+  unsigned char *pixels;
+  int x, y;
+  void *dataBlock;
+  void(*bitmapExit)(void*);
 
-	CPUBitmap(int width, int height, void *d = NULL) {
-		pixels = new unsigned char[width*height*4];
-		x = width;
-		y = height;
-		dataBlock = d;
-	}
+  CPUBitmap(int width, int height, void *d = NULL) {
+    pixels = new unsigned char[width*height*4];
+    x = width;
+    y = height;
+    dataBlock = d;
+  }
 
-	~CPUBitmap() {
-		delete[] pixels;
-	}
+  ~CPUBitmap() {
+    delete[] pixels;
+  }
 
-	unsigned char* get_ptr(void) const { return pixels; }
-	long image_size(void) const { return x*y*4; }
+  unsigned char* get_ptr(void) const { return pixels; }
+  long image_size(void) const { return x*y*4; }
 
-	void display_and_exit(void(*e)(void*) = NULL) {
+  void display_and_exit(void(*e)(void*) = NULL) {
 
-		CPUBitmap **bitmap = get_bitmap_ptr();
-		*bitmap = this;
-		bitmapExit = e;
-		// A bug in the Windows GLUT implementation prevents us from passing zero arguments to glutInit().
-		int c = 1;
-		char* dummy = "";
-		glutInit(&c, &dummy);
-		glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-		glutInitWindowSize(x, y);
-		glutCreateWindow("bitmap");
-		glutKeyboardFunc(Key);
-		glutDisplayFunc(Draw);
-		glutMainLoop();
-	}
+    CPUBitmap **bitmap = get_bitmap_ptr();
+    *bitmap = this;
+    bitmapExit = e;
+    // A bug in the Windows GLUT implementation prevents us from passing zero arguments to glutInit().
+    int c = 1;
+    char* dummy = "";
+    glutInit(&c, &dummy);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutInitWindowSize(x, y);
+    glutCreateWindow("bitmap");
+    glutKeyboardFunc(Key);
+    glutDisplayFunc(Draw);
+    glutMainLoop();
+  }
 
-	// Static method used for glut callbacks
-	static CPUBitmap** get_bitmap_ptr(void) {
-		static CPUBitmap *gBitmap;
-		return &gBitmap;
-	}
+  // Static method used for glut callbacks
+  static CPUBitmap** get_bitmap_ptr(void) {
+    static CPUBitmap *gBitmap;
+    return &gBitmap;
+  }
 
-	// Static method used for glut callbacks
-	static void Key(unsigned char key, int x, int y) {
-		switch (key) {
-		case 27:
-			CPUBitmap *bitmap = *(get_bitmap_ptr());
-			if ( (bitmap->dataBlock != NULL) && (bitmap->bitmapExit != NULL) )
-				bitmap->bitmapExit(bitmap->dataBlock);
-			exit(0);
-		}
-	}
+  // Static method used for glut callbacks
+  static void Key(unsigned char key, int x, int y) {
+    switch (key) {
+      case 27:
+        CPUBitmap *bitmap = *(get_bitmap_ptr());
+        if ((bitmap->dataBlock != NULL) && (bitmap->bitmapExit != NULL))
+          bitmap->bitmapExit(bitmap->dataBlock);
+        exit(0);
+    }
+  }
 
-	// Static method used for glut callbacks
-	static void Draw(void) {
-		CPUBitmap *bitmap = *(get_bitmap_ptr());
-		glClearColor(0.0, 0.0, 0.0, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glDrawPixels(bitmap->x, bitmap->y, GL_RGBA, GL_UNSIGNED_BYTE, bitmap->pixels);
-		glFlush();
-	}
+  // Static method used for glut callbacks
+  static void Draw(void) {
+    CPUBitmap *bitmap = *(get_bitmap_ptr());
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawPixels(bitmap->x, bitmap->y, GL_RGBA, GL_UNSIGNED_BYTE, bitmap->pixels);
+    glFlush();
+  }
 
 };
 
